28c9e1865880341861514c93af9abc960dee698c
khoahuynh88 <khoahuynh1088@gmail.com>
2023-07-26 11:52:37 -0500

    finish

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import FullCalendar from "@fullcalendar/react";
 import daygridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
-import DesktopDateTimePicker from 'react-datetime-picker';
 
+import CreateEventComponent from './CreateEventComponent';
 
 function App() {
   const [events, setEvents] = useState([]);
@@ -14,38 +14,11 @@ function App() {
   const session = useSession(); // tokens, when session exists we have a user
   const supabase = useSupabaseClient(); // talk to supabase!
   const { isLoading } = useSessionContext();
-
-  const [ start, setStart ] = useState(new Date());
-    const [ end, setEnd ] = useState(new Date());
-    const [ eventName, setEventName ] = useState("");
-
+  const [showComponent, setShowComponent] = useState(false);
    
   var showevent=[];
 
-  const [isOnline, setIsOnline] = useState(navigator.onLine);
-  		
-  	useEffect(() => {
-    	function onlineHandler() {
-      		setIsOnline(true);
-    	}
-	
-    	function offlineHandler() {
-      		setIsOnline(false);
-    	}
-	
-    	window.addEventListener("online", onlineHandler);
-    	window.addEventListener("offline", offlineHandler);
 
-	
-    	return () => {
-      		window.removeEventListener("online", onlineHandler);
-      		window.removeEventListener("offline", offlineHandler);
-    	};
-  	}, []);
-    
-    console.log("online status "+isOnline);
-
-    
 
   if(isLoading) {
     return <></>
@@ -94,12 +67,12 @@ function App() {
 
 var islist;
 //Convert eventlist to JSON object
-if (events.length!=0){islist=true;}
+if (events.length!==0){islist=true;}
 //console.log(islist)
 //console.log(events.items[4].start.dateTime)
 //console.log(events.items[0].summary)
 // var showevent=[];
-if(islist==true){
+if(islist===true){
 for (var i=0; i<events.items.length;i++){
   showevent.push({ 
     title: events.items[i].summary,
@@ -109,34 +82,9 @@ for (var i=0; i<events.items.length;i++){
 };} 
 
 
-async function createCalendarEvent() {
-  console.log("Creating calendar event");
-  const event = {
-    'summary': eventName,
-    'start': {
-      'dateTime': start.toISOString(), // Date.toISOString() ->
-      'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
-    },
-    'end': {
-      'dateTime': end.toISOString(), // Date.toISOString() ->
-      'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
-    }
-  }
-  await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
-    method: "POST",
-    headers: {
-      'Authorization':'Bearer ' + session.provider_token // Access token for google
-    },
-    body: JSON.stringify(event)
-  }).then((data) => {
-    return data.json();
-  }).then((data) => {
-    console.log(data);
-    alert("Event created, check your Google Calendar!");
-  });
-  
-}
-
+function handleClick () {
+  setShowComponent(!showComponent); // Toggle the value of showComponent on each click
+};
 
   return (
     <div className="App">
@@ -144,20 +92,6 @@ async function createCalendarEvent() {
   {session ?
     <>
       <h2>Hey there {session.user.email}</h2>
-      <p>Start of your event</p>
-      <DesktopDateTimePicker onChange={setStart} value={start} 
-      calendarClassName="rasta-stripes"/>
-      <p>End of your event</p>
-      <DesktopDateTimePicker onChange={setEnd} value={end} />
-      <p>Event name</p>
-      <input type="text" onChange={(e) => setEventName(e.target.value)} />
-      
-      <hr />
-      <button onClick={() => createCalendarEvent()}>Create Calendar Event</button>
-      <hr />
-      
-      <button onClick={() => listCalendarEvent()}>List Calendar Event</button>
-      <hr />
       <button onClick={() => signOut()}>Sign Out</button>
     </>
     :
@@ -165,22 +99,36 @@ async function createCalendarEvent() {
       <button onClick={() => googleSignIn()}>Sign In With Google</button>
     </> 
   }
-  </div>
+      </div>
+      <div>
+      {showComponent ? <CreateEventComponent /> : <p></p>}
+      </div>
          <FullCalendar
         editable
         selectable
-     
         events={showevent
           
         }
 
+        customButtons={{
+          myCustomButton: {
+            text: '+',
+            click: function(){handleClick()}
+          },
+          listEvent: {
+            text: 'List Event',
+            click: function(){listCalendarEvent()}
+          },
+        }}
+        
         headerToolbar={{
-          start: "today prev next",
-          end: "dayGridMonth dayGridWeek dayGridDay",
+          start: "myCustomButton today prev next",
+          end: "listEvent dayGridMonth dayGridWeek dayGridDay",
         }}
         plugins={[daygridPlugin, interactionPlugin]}
         views={["dayGridMonth", "dayGridWeek", "dayGridDay"]}
       />
+
     </div>
   );
   }
--- src/CreateEventComponent.jsx
+++ src/CreateEventComponent.jsx
@@ -1,53 +1,70 @@
-import React, { Component } from 'react';
-import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
+import React from 'react';
+import { useSession, useSessionContext } from '@supabase/auth-helpers-react';
 import DateTimePicker from 'react-datetime-picker';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
+import 'react-datetime-picker/dist/DateTimePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-clock/dist/Clock.css';
+
 
  const CreateEventComponent = () => {
     const [ start, setStart ] = useState(new Date());
     const [ end, setEnd ] = useState(new Date());
     const [ eventName, setEventName ] = useState("");
-    const [ eventDescription, setEventDescription ] = useState("");
+
+    const event = {
+      'summary': eventName,
+      'start': {
+        'dateTime': start.toISOString(), // Date.toISOString() ->
+        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
+      },
+      'end': {
+        'dateTime': end.toISOString(), // Date.toISOString() ->
+        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
+      }
+    }
+ 
   
     const session = useSession(); // tokens, when session exists we have a user
-    const supabase = useSupabaseClient(); // talk to supabase!
+    //const supabase = useSupabaseClient(); // talk to supabase!
     const { isLoading } = useSessionContext();
-    
+
+    const [showForm, setShowForm] = useState(true);
+
+    const [isOnline, setIsOnline] = useState(navigator.onLine);
+  		
+  	useEffect(() => {
+      console.log("This is local store "+localStorage.getItem('items'));
+      offlineFetch();
+    	function onlineHandler() {
+      		setIsOnline(true);
+    	}
+	
+    	function offlineHandler() {
+      		setIsOnline(false);
+    	}
+	
+    	window.addEventListener("online", onlineHandler);
+    	window.addEventListener("offline", offlineHandler);
+
+	
+    	return () => {
+      		window.removeEventListener("online", onlineHandler);
+      		window.removeEventListener("offline", offlineHandler);
+    	};
+  	}, []);
+
+
     if(isLoading) {
       return <></>
     }
-  
-    async function googleSignIn() {
-      const { error } = await supabase.auth.signInWithOAuth({
-        provider: 'google',
-        options: {
-          scopes: 'https://www.googleapis.com/auth/calendar'
-        }
-      });
-      if(error) {
-        alert("Error logging in to Google provider with Supabase");
-        console.log(error);
-      }
-    }
-  
-    async function signOut() {
-      await supabase.auth.signOut();
-    }
-  
+
+    console.log('check online'+isOnline)
     async function createCalendarEvent() {
-      console.log("Creating calendar event");
-      const event = {
-        'summary': eventName,
-        'description': eventDescription,
-        'start': {
-          'dateTime': start.toISOString(), // Date.toISOString() ->
-          'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
-        },
-        'end': {
-          'dateTime': end.toISOString(), // Date.toISOString() ->
-          'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
-        }
-      }
+      //console.log("Creating calendar event");
+     
+      //check online
+      if(isOnline===true){
       await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
         method: "POST",
         headers: {
@@ -59,34 +76,56 @@ import { useState } from 'react';
       }).then((data) => {
         console.log(data);
         alert("Event created, check your Google Calendar!");
-      });
+      });}else {
+        localStorage.setItem('items', JSON.stringify(event)); 
+        console.log("This is local store "+localStorage.getItem('items'));}
+
     }
-  
-    console.log(session);
-    console.log(start);
-    console.log(eventName);
-    console.log(eventDescription);
+
+    async function offlineFetch() {
+      while (localStorage.getItem('items')!==null && isOnline===true){ 
+        const items = JSON.parse(localStorage.getItem('items'));
+        await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
+        method: "POST",
+        headers: {
+          'Authorization':'Bearer ' + session.provider_token // Access token for google
+        },
+        body: JSON.stringify(items)
+      }).then((data) => {
+        return data.json();
+      }).then((data) => {
+        console.log(data);
+        alert("Event created, after offline");
+      });
+      localStorage.removeItem('items');
+    
+    }
+    }
+    // console.log(session);
+    // console.log(start);
+    // console.log(eventName);
+    // console.log(eventDescription);
 return ( <div className="App">
-<div style={{width: "400px", margin: "30px auto"}}>
-  {session ?
+<div>
+  { showForm?
     <>
-      <h2>Hey there {session.user.email}</h2>
-      <p>Start of your event</p>
-      <DateTimePicker onChange={setStart} value={start} />
+      
+      <DateTimePicker       
+      onChange={setStart} value={start} />
       <p>End of your event</p>
       <DateTimePicker onChange={setEnd} value={end} />
       <p>Event name</p>
       <input type="text" onChange={(e) => setEventName(e.target.value)} />
       <p>Event description</p>
-      <input type="text" onChange={(e) => setEventDescription(e.target.value)} />
-      <hr />
-      <button onClick={() => createCalendarEvent()}>Create Calendar Event</button>
+      
+      <button onClick={() => createCalendarEvent()}>submit</button>
+      <button onClick={() => setShowForm(false)}>Exit</button>
       <p></p>
-      <button onClick={() => signOut()}>Sign Out</button>
+     
     </>
     :
     <>
-      <button onClick={() => googleSignIn()}>Sign In With Google</button>
+      
     </>
   }
 </div>
